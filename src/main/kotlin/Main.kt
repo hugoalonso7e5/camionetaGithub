dd2a7df0af071a472ec1da84030fc6e6827c6d13
hugo [itb] alonso velasco <hugo.alonso.7e5@itb.cat>
2023-12-22 16:16:55 +0100

    EXERCICI ACABAT

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ import java.math.RoundingMode
 fun main() {
     var precioFurgo=73490
     val precioFinal:Double
-    println("Hola Don Usuario que tal estas\n" +
+    println("Hola Don Usuario, que tal estas\n" +
             "El caso es que qué furgoneta tienes, una (1)Volkswagen Grand California Normal\n" +
             "o una (2)Volkswagen Grand California Camper Full Equip?\n" +
             "${CYAN}1) Tengo una ${CYAN_BRIGHT}Volkswagen Grand California Normal\n" +
@@ -16,23 +16,21 @@ fun main() {
 
     precioFurgo+=calcularPreuBase()
 
-    println("Vale entiendo no esta nada mal la verdad.\n" +
+    println("Vale entiendo, no esta nada mal la verdad.\n" +
             "Hace cuantos años la tienes??")
 
     val edad = llegirInt()
 
-    println("Vale entiendo\n" +
-            "Sabiendo que tu furgoneta cuesta $YELLOW_BOLD_BRIGHT$precioFurgo$RESET, cuantos ${WHITE_BOLD_BRIGHT}kilometros$RESET has recorrido?")
+    println("Sabiendo que tu furgoneta cuesta $YELLOW_BOLD_BRIGHT$precioFurgo$RESET, cuantos ${WHITE_BOLD_BRIGHT}kilometros$RESET has recorrido?")
 
     val kilometres = llegirDouble()
 
     precioFinal=calcularPreuActual(kilometres, precioFurgo,edad)
 
-    println("Vale entiendo\n" +
-            "el caso")
+    println("Vale entiendo\n")
 
     println("${YELLOW}Tu furgoneta vale la friolera de $RESET$YELLOW_BOLD_BRIGHT${BigDecimal(precioFinal).setScale(2, RoundingMode.FLOOR)}$RESET")
-    println("asique aqui tienes tu furgoneta pintada\n" +
+    println("Aqui tienes tu furgoneta pintada\n" +
             RED_BOLD +
             " ================\\\n" +
             " |----------||@  \\\\   ___\n" +
